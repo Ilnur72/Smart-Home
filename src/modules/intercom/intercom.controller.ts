984e73e8267d9f452f3f5bcdadd3491039b2367c
Ilnur72 <umirbayev72@gmail.com>
2024-12-04 13:02:59 +0500

    Update intercom.controller.ts

--- src/modules/intercom/intercom.controller.ts
+++ src/modules/intercom/intercom.controller.ts
@@ -22,6 +22,12 @@ import { IsLoggedIn } from '../../shared/guards/is-loggedin.guard';
 import { HasRole } from '../../shared/guards/has-roles.guard';
 import { ResponseIntercomDto } from './dto/intercom.dto';
 import { MessageService } from '../../i18n/message.service';
+import axios from 'axios';
+import * as https from 'https';
+// import AxiosDigestAuth from '@mhoc/axios-digest-auth';
+import DigestClient from 'digest-fetch';
+
+import crypto from 'crypto';
 
 @ApiTags('Intercom')
 @Controller('intercom')
@@ -31,8 +37,73 @@ export class IntercomController {
     private readonly messageService: MessageService,
   ) {}
 
+  @Get('opendoor')
+  async openDoor() {
+    const data = `<RemoteControlDoor version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema">
+      <doorNo min="1" max="1"></doorNo>
+      <cmd>open</cmd>
+    </RemoteControlDoor>`;
+
+    // HTTPS Agent with SSL verification disabled
+    const agent = new https.Agent({
+      rejectUnauthorized: false,
+    });
+
+    // Digest Authentication initialization
+    const digestAuth = new DigestClient('admin', '12345678a', {
+      algorithm: 'MD5',
+    });
+
+    try {
+      // Perform the request with Digest Authentication
+      const response = await digestAuth.fetch({
+        method: 'PUT',
+        url: 'https://172.25.25.96/ISAPI/AccessControl/RemoteControl/door/1',
+        headers: {
+          'Content-Type': 'application/xml',
+        },
+        // auth: {}
+
+        httpsAgent: agent, // Attach the HTTPS agent to the request
+        data: data,
+      });
+
+      console.log('Response:', response.formData);
+      return response.formData; // Return the response to the client
+    } catch (error) {
+      console.log(error);
+      console.error('Error:', error.message || error.response?.data);
+      throw error; // Throw the error for proper handling
+    }
+  }
+
+  private calculateDigestAuth(
+    username: string,
+    password: string,
+    uri: string,
+    method: string,
+    nonce: string,
+    cnonce: string,
+    nc: string,
+  ): string {
+    const ha1 = crypto
+      .createHash('md5')
+      .update(`${username}:DS-9076C1B6:${password}`)
+      .digest('hex');
+    const ha2 = crypto
+      .createHash('md5')
+      .update(`${method}:${uri}`)
+      .digest('hex');
+    const response = crypto
+      .createHash('md5')
+      .update(`${ha1}:${nonce}:${nc}:${cnonce}:auth:${ha2}`)
+      .digest('hex');
+
+    return `Digest username="${username}", realm="DS-9076C1B6", nonce="${nonce}", uri="${uri}", qop="auth", algorithm="MD5", response="${response}", nc="${nc}", cnonce="${cnonce}"`;
+  }
+
   @UseGuards(IsLoggedIn, HasRole)
-  @SetRoles(UserRole.ADMIN)
+  @SetRoles(UserRole.SYSTEM_ADMIN)
   @Post()
   @ApiResponse({
     status: 201,
@@ -96,7 +167,7 @@ export class IntercomController {
   }
 
   @Get(':id')
-  @SetRoles(UserRole.ADMIN, UserRole.USER)
+  @SetRoles(UserRole.SYSTEM_ADMIN, UserRole.USER)
   @ApiResponse({
     status: 200,
     description: 'Single Intercom details',
@@ -122,7 +193,7 @@ export class IntercomController {
   }
 
   @UseGuards(IsLoggedIn, HasRole)
-  @SetRoles(UserRole.ADMIN)
+  @SetRoles(UserRole.SYSTEM_ADMIN)
   @Put(':id')
   @ApiResponse({
     status: 200,
@@ -151,7 +222,7 @@ export class IntercomController {
   }
 
   @UseGuards(IsLoggedIn, HasRole)
-  @SetRoles(UserRole.ADMIN)
+  @SetRoles(UserRole.SYSTEM_ADMIN)
   @Delete(':id')
   @ApiResponse({
     status: 200,
